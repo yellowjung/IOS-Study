2a91405ec6722c4d017c6de9985c201ff3eb5e46
YoungdonJung <yeavov@gmail.com>
2021-02-15 20:40:18 +0900

    Auto Layout Study

--- IOS_Study/ViewController.swift
+++ IOS_Study/ViewController.swift
@@ -15,22 +15,25 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        priceLabel.text = "₩\(currentValue)"
+        refresh()
     }
 
     @IBAction func Refresh(_ sender: Any) {
         let message = "가격은 \(currentValue)입니다.";
-        
         let alert = UIAlertController(title : "Hello",
                                       message: message, preferredStyle: .alert)
-        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        let action = UIAlertAction(title: "OK", style: .default, handler: { action in
+            self.refresh()
+        })
         alert.addAction(action);
         present(alert, animated: true, completion: nil)
         
+    }
+    
+    func refresh(){
         let randomPrice = arc4random_uniform(10000) + 1
         currentValue = Int(randomPrice);
         priceLabel.text = "₩\(currentValue)"
     }
-    
 }
 
