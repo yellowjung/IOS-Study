304191edd11f00d6370ac6e412be10f267fb7f2f
YoungdonJung <yeavov@gmail.com>
2021-03-01 15:59:44 +0900

    Add Table View

---
+++ IOS_Study/BountyViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BountyViewController.swift
+//  IOS_Study
+//
+//  Created by 정영돈 on 2021/03/01.
+//
+
+import UIKit
+
+class BountyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
