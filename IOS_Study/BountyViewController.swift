fdb97f4d11f024363fa07761b4feef971994f5e0
YoungdonJung <yeavov@gmail.com>
2021-03-04 21:21:14 +0900

    Set scale for any version

--- IOS_Study/BountyViewController.swift
+++ IOS_Study/BountyViewController.swift
@@ -13,7 +13,6 @@ class BountyViewController: UIViewController,
                             UICollectionViewDelegateFlowLayout
 {
 
-
     //MVVM
     
     //Model
@@ -37,8 +36,6 @@ class BountyViewController: UIViewController,
         if segue.identifier == "showDetail"{
             let vc = segue.destination as? DetailViewController
             if let index = sender as? Int{
-                
-
                 let bountyInfo = viewModel.bountyInfo(at: index)
                 vc?.viewModel.update(model: bountyInfo)
             }
@@ -70,7 +67,6 @@ class BountyViewController: UIViewController,
         return cell
     }
     
-    
     //UICollectionViewDelegate
     //셀이 클릭되었을 때 어떻게 하나
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
@@ -80,45 +76,17 @@ class BountyViewController: UIViewController,
     
     //UICollectionViewDelegateFlowLayout
     //cell size를 어떻게 계산해서 표현하나 (기기별 사이즈)
-    
-    
-    
-    //UITableViewDataSource
-    
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return viewModel.numOfBountyInfoList
-//    }
-//
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ListCell else{
-//            return UITableViewCell()
-//        }
-//        let bountyInfo = viewModel.bountyInfo(at: indexPath.row)
-//        cell.update(info: bountyInfo)
-//
-//        return cell;
-//    }
-//
-//    //UITableViewDelegate
-//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        print("--> \(indexPath.row)")
-//        performSegue(withIdentifier: "showDetail", sender: indexPath.row)
-//    }
-}
-
-class ListCell: UITableViewCell {
-    @IBOutlet weak var imgView: UIImageView!
-    @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var bountyLabel: UILabel!
-
-    func update(info: BountyInfo){
-        imgView.image = info.image
-        nameLabel.text = info.name
-        bountyLabel.text = "\(info.bounty)"
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+       
+        let itemSpacing: CGFloat = 10
+        let textAreaHeight: CGFloat = 65
+        
+        let width: CGFloat = (collectionView.bounds.width - itemSpacing) / 2
+        let height: CGFloat = width * 10 / 7 + textAreaHeight
+        
+        return CGSize(width: width, height: height)
     }
 }
-
-
 
 class BountyViewModel{
     let bountyInfoList: [BountyInfo] = [
