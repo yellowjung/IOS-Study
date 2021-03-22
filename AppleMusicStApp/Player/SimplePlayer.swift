df85a18be5bf987883caf9d38aa2ecb242db1f72
YoungdonJung <yeavov@gmail.com>
2021-03-22 20:31:55 +0900

    Make simple player

--- AppleMusicStApp/Player/SimplePlayer.swift
+++ AppleMusicStApp/Player/SimplePlayer.swift
@@ -10,49 +10,51 @@ import AVFoundation
 
 class SimplePlayer {
     // TODO: 싱글톤 만들기, 왜 만드는가?
-    
+    // 앱 전반에서 한 가지로 공유하기 위해서
+    static let shared = SimplePlayer()
     
     private let player = AVPlayer()
 
     var currentTime: Double {
         // TODO: currentTime 구하기
-        return 0.0
+        return player.currentItem?.currentTime().seconds ?? 0
     }
     
     var totalDurationTime: Double {
         // TODO: totalDurationTime 구하기
-        return 0.0
+        return player.currentItem?.duration.seconds ?? 0
     }
     
     var isPlaying: Bool {
         // TODO: isPlaying 구하기
-        return false
+        return player.isPlaying
     }
     
     var currentItem: AVPlayerItem? {
         // TODO: currentItem 구하기
-        return nil
+        return player.currentItem
     }
     
     init() { }
     
     func pause() {
         // TODO: pause구현
-        
+        player.pause()
     }
     
     func play() {
         // TODO: play구현
-        
+        player.play()
     }
     
     func seek(to time:CMTime) {
         // TODO: seek구현
-        
+        player.seek(to: time)
     }
     
     func replaceCurrentItem(with item: AVPlayerItem?) {
         // TODO: replace current item 구현
+        player.replaceCurrentItem(with: item)
         
     }
     
